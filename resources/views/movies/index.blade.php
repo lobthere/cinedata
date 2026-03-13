dc3df5271a83601403f9041553dc052bbc714faa
Ange N'DAH <angemehola@gamil.com>
2026-03-13 13:12:37 +0100

    edit of html

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,11 +1,21 @@
-<h1>Liste des films</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Liste des films</h1>
 
-<ul>
-@foreach ($movies as $movie)
-    <li>
-        <a href="/movies/{{ $movie['id'] }}">{{ $movie['title'] }}</a>
-    </li>
+    <ul>
+    @foreach ($movies as $movie)
+        <li>
+            <a href="/movies/{{ $movie['id'] }}">{{ $movie['title'] }}</a>
+        </li>
 
-@endforeach
+    @endforeach
 
-</ul>
+    </ul>
+</body>
+</html>
--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -1,5 +1,15 @@
-<h1>{{ $movie['title'] }}</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>{{ $movie['title'] }}</h1>
 
-<p>{{ $movie['content'] }}</p>
+    <p>{{ $movie['content'] }}</p>
 
-<a href="/movies">Retour</a>
+    <a href="/movies">Retour</a>
+</body>
+</html>
