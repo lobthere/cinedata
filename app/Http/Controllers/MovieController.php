a710caab63781f743edd7237d1a14b2e58a9c8fd
lobthere <lobthere@protonmail.com>
2026-06-01 13:34:45 +0200

    removed old function

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,21 +4,6 @@ namespace App\Http\Controllers;
 use App\Models\Movies;
 
 class MovieController extends Controller{
-    private function getMovies(){
-    /*
-        contient notre liste de film et la retourne
-    */
-        return [
-            ['title' => 'Armagedon', 'content' => 'Un film utilisé par la nasa pour trouver des erreurs', 'img' => 'https://upload.wikimedia.org/wikipedia/en/f/fc/Armageddon-poster06.jpg'],
-            ['title' => '1917', 'content' => 'un film sur la premiere guerre mondial', 'img' => 'https://upload.wikimedia.org/wikipedia/en/f/fe/1917_%282019%29_Film_Poster.jpeg'],
-            ['title' => 'moi moche et mechant', 'content' => 'des minions et gru', 'img' => 'https://upload.wikimedia.org/wikipedia/fr/7/7a/Moi-moche-et-mechant.jpg'],
-            ['title' => 'zootopie', 'content' => 'un zoo mais ils parlent', 'img' => 'https://duckduckgo.com/i/07228fb46dac0f09.jpg'],
-            ['title' => 'interstellar', 'content' => 'un film pour casser la tête', 'img' => 'test'],
-            ['title' => 'Mon voisin totoro', 'content' => 'film mignon ghibli', 'img' => 'test'],
-            ['title' => 'Jurrassic parc', 'content' => 'un film de dinosaures', 'img' => 'test']
-        ];// \App\Models\Article::create(['name' =>'Premier article','content' =>'Contenu de mon premier article.', 'img' => ]);
-    }
-
     public function index(){
         //retourne la liste des films dans la variable nommee 'movies' quand il est appele
         //return the list of the movies in the var 'movies' when called
