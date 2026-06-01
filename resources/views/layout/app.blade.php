7633b057ad15ea593332a40b6a1475653f4d23d9
Ange N'DAH <angemehola@gamil.com>
2026-06-01 15:05:02 +0200

    Ajout des entetes et du css

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,6 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
     @yield('head')
+   
+<header class="main-header">
+    <div class="logo"></div>
+    
+    <nav class="navbar">
+<a href="/movies" class="nav-logo">Cinédata</a>
+<ul class="nav-links">
+     <li><a href="/movies">Films</a></li>
+     <li><a href="#">Cinéma</a></li>
+     <li><a href="#">Acteurs</a></li>
+     <li><a href="#">Prochaines sorties</a></li>
+     <li><a href="#">À propos</a></li>
+     <li><a href="#">Connexion</a></li>
+</ul>
+</nav>
+</header>
+
 </head>
 <body>
     <img src="{{ $movie['image'] ?? 'https://images.unsplash.com/photo-1536440136628-849c177e76a1?w=500' }}" alt="Affiche du film" class="banner-cinema">
