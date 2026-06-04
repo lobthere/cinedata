468e742b1e8b1a8f925337bfb2134c6d2c5cc373
lobthere <lobthere@protonmail.com>
2026-06-04 14:54:25 +0200

    added the comments controller

---
+++ app/Http/Controllers/Comments.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Comments extends Controller
+{
+    public function addComments(Request $request, $id){
+
+        $request -> validate([
+            'foreignId' => 'required',
+            'username' => 'required',
+            'comment' => 'required'
+        ]);
+
+        Comments::create(['foreignId' => $id,
+                        'username' => $request->username,
+                        'comment' => $request->comment
+                    ]);
+
+        return redirect('/movies/' . $id);
+    }
+}
